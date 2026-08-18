cf82fb76a2f189ca66e62deeb70a23bcf2c9b420
baenreiiryn <baenreiiryn@gmail.com>
2026-08-18 22:44:03 +0900

    feat: export advanced mechanics and vehicles

--- packages/schema/src/index.ts
+++ packages/schema/src/index.ts
@@ -1,8 +1,10 @@
 export const CANONICAL_SCHEMA_VERSION = 1 as const;
 
 export * from "./primitives.js";
+export * from "./mechanics.js";
 export * from "./activity.js";
 export * from "./monster.js";
+export * from "./vehicle.js";
 export * from "./item.js";
 export * from "./spell.js";
 export * from "./feature.js";
