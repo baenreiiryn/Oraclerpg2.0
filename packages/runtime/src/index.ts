6eaec97ce5e4e1b01a04fc8681c87e024d501630
baenreiiryn <baenreiiryn@gmail.com>
2026-08-20 10:00:01 +0900

    AI-5: export world state runtime

--- packages/runtime/src/index.ts
+++ packages/runtime/src/index.ts
@@ -2,3 +2,6 @@ export * from "./turn-orchestrator.js";
 export * from "./rules/types.js";
 export * from "./rules/compendium-index.js";
 export * from "./rules/bridge.js";
+export * from "./state/types.js";
+export * from "./state/world-state-service.js";
+export * from "./state/context-source.js";
