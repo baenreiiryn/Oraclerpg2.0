a0e68cde6ee8139f5ec2fd056226e048ead07b5b
baenreiiryn <baenreiiryn@gmail.com>
2026-08-20 23:22:52 +0900

    chore(i18n): inventory level 9 spells

---
+++ packages/content/test/spell-level9-inventory.test.js
@@ -0,0 +1,20 @@
+import fs from "node:fs";
+import path from "node:path";
+import test from "node:test";
+import { fileURLToPath } from "node:url";
+
+const here = path.dirname(fileURLToPath(import.meta.url));
+const canonical = JSON.parse(fs.readFileSync(path.join(here, "../data/srd-5.2/spells.json"), "utf8"));
+const spells = canonical.items.filter((spell) => spell.data?.level === 9);
+
+test("inventory level-9 spells", () => {
+  console.log("LEVEL9_COUNT", spells.length);
+  for (const spell of spells) {
+    console.log("LEVEL9_SPELL", JSON.stringify({
+      canonicalId: spell.canonicalId,
+      name: spell.name,
+      text: spell.data?.text,
+      activities: spell.data?.activities
+    }));
+  }
+});
