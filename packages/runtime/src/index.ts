cfdf0b00a0c63221fe5e272b67674e4ab0b9514f
baenreiiryn <baenreiiryn@gmail.com>
2026-08-20 11:25:15 +0900

    Export AI-9 GM runtime

--- packages/runtime/src/index.ts
+++ packages/runtime/src/index.ts
@@ -1,4 +1,5 @@
 export * from "./turn-orchestrator.js";
+export * from "./gm-runtime.js";
 export * from "./rules/types.js";
 export * from "./rules/compendium-index.js";
 export * from "./rules/bridge.js";
